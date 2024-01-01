6d11561053fa155e069a3f8a2d4bb8199bb134f1
DFori <daniel.tatama@binghamuni.edu.ng>
2024-01-01 20:05:27 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -35,14 +35,14 @@ function st(fun, time = 1) {
 }
 
 var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) {
-    /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
+// function openFullscreen() {
+//   if (elem.requestFullscreen) {
+//     elem.requestFullscreen();
+//   } else if (elem.webkitRequestFullscreen) {
+//     /* Safari */
+//     elem.webkitRequestFullscreen();
+//   } else if (elem.msRequestFullscreen) {
+//     /* IE11 */
+//     elem.msRequestFullscreen();
+//   }
+// }
